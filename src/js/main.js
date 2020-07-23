168c0a888c5c61283ca3396d50d9269e57b7e8bd
Alex K <contact.alexsophia@gmail.com>
2020-07-23 19:17:56 +0300

    Started working on the button.

--- src/js/main.js
+++ src/js/main.js
@@ -15,14 +15,37 @@ getRandomJoke()
     .then(data => fillInJokes(data));
 
 function fillInJokes(data) {
+    const container = document.getElementById('container');
+
     const joke = document.createElement('div');
     joke.setAttribute('class', 'joke-container');
     joke.innerText = data.joke;
 
-    const container = document.getElementById('container');
     container.appendChild(joke);
+
+    createButton();
 }
 
-function refreshPage(){
-    window.location.reload();
+function createButton() {
+    const container = document.getElementById('container');
+
+    const buttonContainer = document.getElementById('button-container');
+
+    const refreshButton = document.createElement('button');
+    refreshButton.setAttribute('class', 'btn btn-light');
+    refreshButton.setAttribute('id', 'generate-button');
+    refreshButton.innerText = 'Haha! Crack another one!';
+
+    buttonContainer.appendChild(refreshButton);
+    container.appendChild(buttonContainer);
+
+    refreshButton.addEventListener('click', event => {
+        event.preventDefault();
+        
+        refreshButton.remove();
+
+        getRandomJoke()
+            .then(data => fillInJokes(data));
+    })
 }
+
